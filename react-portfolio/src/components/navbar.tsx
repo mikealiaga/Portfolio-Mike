b8c2095241c6176adf3c5feb4cdc90cfe8694082
mikealiaga <mikealiaga97@gmail.com>
2025-02-10 17:46:30 -0600

    Added Nav Bar

---
+++ react-portfolio/src/components/navbar.tsx
@@ -0,0 +1,84 @@
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import IsotypeLogo from "../assets/IsotypeAStudio.svg";
+import NavbarStyles from "../styles/navbarStyles.css";
+
+// Define navigation links
+const navigation = [
+  { name: "Home", href: "#", current: true },
+  { name: "About me", href: "#", current: false },
+  { name: "Portfolio", href: "#", current: false },
+  { name: "Contact", href: "#", current: false },
+];
+
+function classNames(...classes: (string | boolean | undefined)[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Navbar() {
+  return (
+    <Disclosure as="nav" className="bg-neutral-800 rounded-xl">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          
+          {/* Left: Logo */}
+            <img
+              src={IsotypeLogo}
+              alt="Aliaga Studio Logo"
+              className="h-8 w-auto invert"
+            />
+
+          {/* Right: Mobile Menu Button */}
+          <div className="sm:hidden">
+            <DisclosureButton className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-none focus:ring-inset">
+              {({ open }) =>
+                open ? (
+                  <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                )
+              }
+            </DisclosureButton>
+          </div>
+
+          {/* Desktop Navigation */}
+          <div className="hidden sm:flex sm:items-center sm:justify-end">
+            <div className="flex space-x-4">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "rounded-md px-3 py-2 text-sm font-medium"
+                  )}
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pt-2 pb-3">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              className={classNames(
+                item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                "block rounded-md px-3 py-2 text-base font-medium"
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  );
+}
---
+++ react-portfolio/src/pages/contact.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <div className=''>
+      <h1>About Me</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla ducimus sit iste non repudiandae iusto tempora rem voluptatum in! Repellendus, ut eveniet! Dolorem iste perspiciatis dolor ipsum amet exercitationem dolore!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam deleniti distinctio eum delectus eius sequi blanditiis doloremque quas voluptatibus consectetur atque provident sit commodi maxime, necessitatibus vitae quod recusandae aspernatur.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptas labore. Ratione a nulla repellendus et optio tempora corporis soluta incidunt commodi recusandae molestias unde quibusdam veritatis, pariatur magni ut.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur temporibus neque fuga eaque voluptate maxime, accusamus sint optio! Accusamus quidem earum at fugiat rerum explicabo aliquid officia voluptatum mollitia neque!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore esse laudantium veritatis inventore hic excepturi consectetur pariatur tenetur aperiam sed accusantium fugit, mollitia, in, quaerat culpa! Maxime quod nobis temporibus.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis laborum dignissimos harum exercitationem! Molestias dolorem nisi minus, expedita, aut optio, ipsa assumenda amet nulla doloribus at reprehenderit repellendus accusamus aspernatur.
+      </p>
+    </div>
+  )
+}
+
+export default AboutMe
